6ba52cbee626795e2f42543fcd67f4b73dc0391a
Elena_Chekanina <Elena_Chekanina@epam.com>
2022-12-06 21:22:55 +0300

    added map state tree change

--- src/app2/components/content/page-content/page-content-renderer/map/IeecloudMapRenderer.js
+++ src/app2/components/content/page-content/page-content-renderer/map/IeecloudMapRenderer.js
@@ -85,7 +85,7 @@ export default class IeecloudMapRenderer {
                 'title': mapDataObj.title,
                 icon: eval(mapDataObj.icon)
             }).addTo(scope.#dataMap).on('click', function (e) {
-                const data = {groupId : "", activeNode: scope.#node}
+                const data = {groupId : mapDataObj.id, activeNode: scope.#node}
                 eventBus.emit('IeecloudTableRenderer.rowClick', data, false);
             });
         });
