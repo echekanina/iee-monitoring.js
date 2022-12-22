1272dee99539d67ab9a1d5d3e2899184e965445a
Elena_Chekanina <Elena_Chekanina@epam.com>
2022-12-23 01:35:59 +0300

    fixed btn style

--- src/app2/components/content/page-content/page-content-renderer/IeecloudWidgetRenderer.js
+++ src/app2/components/content/page-content/page-content-renderer/IeecloudWidgetRenderer.js
@@ -42,7 +42,7 @@ export default class IeecloudWidgetRenderer {
 </div>
     
 <div class="btn-group">
-    <a  href="#" role="button" style="padding-left: 0.5rem;" class="btn btn-icon rounded-circle action dropdown-toggle ${(this.#layoutModel.viewActions ? "" : "d-none")}" id="dropdownMenuLink-` + this.#node.id + `-` + this.#layoutModel.id + `"  title="Вид Отображения" data-bs-toggle="dropdown">
+    <a  href="#" role="button" style="padding-left: 0.45rem;" class="btn btn-icon rounded-circle action dropdown-toggle ${(this.#layoutModel.viewActions ? "" : "d-none")}" id="dropdownMenuLink-` + this.#node.id + `-` + this.#layoutModel.id + `"  title="Вид Отображения" data-bs-toggle="dropdown">
                                             <i class="fa-solid fa-table-list"></i>
                                              </a>   
   
