74874d9434ed31f42c3d8f41ae9ae8f6474f345a
Alexey_Ivanov1 <alexey.ivanov.star@gmail.com>
2023-04-07 00:20:41 +0300

    replace some path in dist folder, otherwise some resources couldn't be found

---
+++ build.ps1
@@ -0,0 +1,19 @@
+# https://stackoverflow.com/questions/20949892/using-powershell-as-terminal-in-intellij-idea-ides-like-pycharm-phpstorm-or-rub
+# I updated the powershell.exe path in IntelliJ -> Settings -> Terminal
+# Opened a powershell instance in Admin mode.
+# Executed Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+# Restarted IntelliJ and the issue was solved.
+
+#powershell -Command "(gc dist/index.html -Encoding UTF8) -replace '/assets/', 'assets/' | Out-File -encoding UTF8 dist/index.html"
+#powershell -Command "(gc dist/assets/index.*.js -Encoding UTF8) -replace '/assets/', 'assets/' | Out-File -encoding UTF8 dist/index.html"
+
+# replace index.*.html
+(gc dist/index.html -Encoding UTF8) -replace '/assets/', 'assets/' | Out-File -encoding UTF8 dist/index.html
+
+# replace index.*.js
+$var=Get-ChildItem -Path dist/assets -Recurse -Filter 'index.*.js' | Select-Object FullName
+(gc $var.FullName -Encoding UTF8) -replace '/assets/', 'assets/' | Out-File -encoding UTF8 $var.FullName
+
+# replace index.*.css
+$var=Get-ChildItem -Path dist/assets -Recurse -Filter 'index.*.css' | Select-Object FullName
+(gc $var.FullName -Encoding UTF8) -replace '/assets/', '' | Out-File -encoding UTF8 $var.FullName
