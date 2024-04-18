085137673a553899884e8b2fe98df294eeb8e2dc
Alexey_Ivanov1 <alexey.ivanov.star@gmail.com>
2024-04-18 21:06:49 +0300

    - fixed config.js

--- config.test.js
+++ config.test.js
@@ -1,7 +1,7 @@
 globalThis.import_meta_env = {
     "APP_SERVER_URL":"https://mail.igr.spb.ru/dvm_ui_shell_conf/{%ORG_CODE%}/{%APP_TYPE%}/{%ENV%}/{%APP_CODE%}/",
     "APP_STATIC_STORAGE":"https://mail.igr.spb.ru/monitor_ui_static/{%ENV%}/{%APP_CODE%}/",
-    "APP_SERVER_ROOT_URL":"http://localhost:8080/dvm/api/{%APP_CODE%}",
+    "APP_SERVER_ROOT_URL":"https://mail.igr.spb.ru/dvmtest/api/{%APP_CODE%}",
     "APP_CODE":"{%APP_CODE%}",
     "APP_TYPE":"{%APP_TYPE%}",
     "ORG_CODE":"{%ORG_CODE%}",
