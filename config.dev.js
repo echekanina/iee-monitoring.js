845f980de57c2a0df04e16b8bbaf99cecfc20304
Elena_Chekanina <Elena_Chekanina@epam.com>
2024-04-23 19:33:11 +0300

    single-spa configuration reworked 2

--- config.dev.js
+++ config.dev.js
@@ -1,5 +1,5 @@
 globalThis.import_meta_env = {
-    "APP_SERVER_URL":`https://mail.igr.spb.ru/dvm_ui_shell_conf/{%ORG_CODE%}/{%APP_TYPE%}/{%ENV%}/{%APP_CODE%}/`,
+    "APP_SERVER_URL":"https://mail.igr.spb.ru/dvm_ui_shell_conf/{%ORG_CODE%}/{%APP_TYPE%}/{%ENV%}/{%APP_CODE%}/",
     "APP_STATIC_STORAGE":"https://mail.igr.spb.ru/monitor_ui_static/{%ENV%}/{%APP_CODE%}/",
     "APP_SERVER_ROOT_URL":"https://mail.igr.spb.ru/dvmdev/api/{%APP_CODE%}",
     "APP_CODE":"{%APP_CODE%}",
