ea04a699ab20715c7c635bc97cea8a8cb834660b
Alexey_Ivanov1 <alexey.ivanov.star@gmail.com>
2024-05-03 19:15:37 +0300

    - fixed config.js

--- config.dev.js
+++ config.dev.js
@@ -1,6 +1,6 @@
 globalThis.import_meta_env = {
-    "APP_SERVER_URL":"https://mail.igr.spb.ru/dvm_ui_shell_conf/{%ORG_CODE%}/{%APP_TYPE%}/{%ENV%}/{%APP_CODE%}/",
-    "APP_STATIC_STORAGE":"https://mail.igr.spb.ru/monitor_ui_static/{%ENV%}/{%APP_CODE%}/",
+    "APP_SERVER_URL":"https://mail.igr.spb.ru:6644/ieeplatform/dvm-ui-shell-config/{%ORG_CODE%}/{%APP_TYPE%}/{%ENV%}/{%APP_CODE%}/",
+    "APP_STATIC_STORAGE":"https://mail.igr.spb.ru:6644/ieeplatform/monitor_ui_static/{%ENV%}/{%APP_CODE%}/",
     "APP_SERVER_ROOT_URL":"https://mail.igr.spb.ru/dvmdev/api/{%APP_CODE%}",
     "APP_SERVER_ROOT_LOGIN_URL":"https://mail.igr.spb.ru/dvmdev/api/root",
     "APP_CODE":"{%APP_CODE%}",
